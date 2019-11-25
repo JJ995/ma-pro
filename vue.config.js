d62be76af481b8dcb485b4cf4f2d5a5e332f8d33
Jakob Januschkowetz <j.januschkowetz@pixelart.at>
2019-11-25 22:43:01 +0100

    add basic changed file watcher

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,6 @@
 /*
  * Hooks: https://stackoverflow.com/questions/58325548/how-to-execute-my-own-script-after-every-webpacks-auto-build-in-watch-mode-of-v
+ * Get list of changed files: https://stackoverflow.com/questions/43140501/can-webpack-report-which-file-triggered-a-compilation-in-watch-mode
  */
 
 // Includes
@@ -15,8 +16,8 @@ const plugins = [];
  * Initialize before run hook
  * @param callback
  */
-const beforeRunHook = function(callback) {
-    this.apply = function(compiler) {
+const beforeRunHook = function (callback) {
+    this.apply = function (compiler) {
         if (compiler.hooks && compiler.hooks.watchRun) {
             compiler.hooks.watchRun.tap('webpack-arbitrary-code', callback);
         }
@@ -24,6 +25,26 @@ const beforeRunHook = function(callback) {
 };
 // Add hook
 plugins.push(new beforeRunHook(generateManifest));
+
+/**
+ * Print list of changed files since last build to console
+ */
+class WatchRunPlugin {
+    apply(compiler) {
+        compiler.hooks.watchRun.tap('WatchRun', (comp) => {
+            const changedTimes = comp.watchFileSystem.watcher.mtimes;
+            const changedFiles = Object.keys(changedTimes)
+            .map(file => `\n  ${file}`)
+            .join('');
+            if (changedFiles.length) {
+                console.log("====================================");
+                console.log('NEW BUILD FILES CHANGED:', changedFiles);
+                console.log("====================================");
+            }
+        });
+    }
+}
+plugins.push(new WatchRunPlugin());
 
 module.exports = {
     chainWebpack: config => {
